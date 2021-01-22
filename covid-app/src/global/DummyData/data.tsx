ba3181bf1d7611484f4ee538fb31ce2928867484
kokcheng95 <kokcheng95@gmail.com>
2021-01-23 00:23:16 +0800

    added horinzontal scroll view , added fetch api libraries

--- covid-app/src/global/DummyData/data.tsx
+++ covid-app/src/global/DummyData/data.tsx
@@ -1,36 +1,42 @@
 //Temporarily store data here
 export const ProductDummyData = [
     {
+      "productId":1,
       "title": "News",
       "name": "CNN Acquire BEME",
       "desc": "CNN purchased Casey Neistat's Beme app for $25million.",
       "imageUrl": "https://source.unsplash.com/user/erondu/600x400"
     },
     {
+      "productId":2,
       "title": "Travel",
       "name": "Nomad Lifestyle",
       "desc": "Learn our tips and tricks on living a nomadic lifestyle",
       "imageUrl": "https://source.unsplash.com/user/_vickyreyes/600x400"
     },
     {
+      "productId":3,
       "title": "Development",
       "name": "React and the WP-API",
       "desc": "The first ever decoupled starter theme for React & the WP-API",
       "imageUrl": "https://source.unsplash.com/user/ilyapavlov/600x400"
     },
     {
+      "productId":4,
       "title": "News",
       "name": "CNN Acquire BEME",
       "desc": "CNN purchased Casey Neistat's Beme app for $25million.",
       "imageUrl": "https://source.unsplash.com/user/erondu/600x400"
     },
     {
+      "productId":5,
       "title": "Travel",
       "name": "Nomad Lifestyle",
       "desc": "Learn our tips and tricks on living a nomadic lifestyle",
       "imageUrl": "https://source.unsplash.com/user/_vickyreyes/600x400"
     },
     {
+      "productId":6,
       "title": "Development",
       "name": "React and the WP-API",
       "desc": "The first ever decoupled starter theme for React & the WP-API",
